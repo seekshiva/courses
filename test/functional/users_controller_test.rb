a2adfe8c34c5243e4228a5fc0c2a4afec02bd6e4
Shiva Nandan <seekshiva@gmail.com>
2013-12-21 12:47:05 +0530

    test(functional) - Fix users_controller functional test
    Following changes were made:
    * remove `designation` field, which is not part of users model
    * rename `mobile` field to `phone`
    * rename `profile_pic` field to `avatar_id`
    
    Note:
    All functional test for admin controllers will fail right now. Admin pages
    give a 302 if the user isn't authenticated as admin, and so the tests fail.
    
    Testing admin pages must be done with an test admin user. CI tests will
    continue to fail until this is resolved.

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -18,7 +18,7 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should create user" do
     assert_difference('User.count') do
-      post :create, user: { designation: @user.designation, email: @user.email, mobile: @user.mobile, name: @user.name, profile_pic: @user.profile_pic }
+      post :create, user: { name: @user.name, department: @user.department, email: @user.email, phone: @user.phone, avatar: @user.avatar }
     end
 
     assert_redirected_to user_path(assigns(:user))
@@ -35,7 +35,7 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should update user" do
-    put :update, id: @user, user: { designation: @user.designation, email: @user.email, mobile: @user.mobile, name: @user.name, profile_pic: @user.profile_pic }
+    put :update, id: @user, user: { name: @user.name, department: @user.department, email: @user.email, phone: @user.phone, avatar: @user.avatar }
     assert_redirected_to user_path(assigns(:user))
   end
 
