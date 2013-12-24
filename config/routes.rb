a53930b35fa8b678506e2d9a23c909e8d74902f6
Shiva Nandan <seekshiva@gmail.com>
2013-12-25 02:09:27 +0530

    fix(router) - Fix missing :controller (ArgumentError)

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,5 @@
 Courses::Application.routes.draw do
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
   namespace :admin do
     resources :users
     resources :faculties
@@ -67,7 +16,7 @@ Courses::Application.routes.draw do
       get ":tab" => "courses#show"
     end
     resources :books, :authors, :book_authors, :references
-    get 'switch_to' => 'users#switch_to'
+    get 'switch_to', to: 'users#switch_to'
   end
 
   resources :users
@@ -85,19 +34,14 @@ Courses::Application.routes.draw do
 
   get 'me', to: 'home#me'
   get 'admin', to: 'admin/departments#index'
-  get 'login' => 'home#index'
+  get 'login', to: 'home#index'
   get 'signout', to: 'home#signout'
 
-  get ':slug.json' => '#{:slug}s#index'
-  get ':slug' => '#{:slug}s#index'
+  get ':slug.json', to: "#{:slug}s#index"
+  get ':slug', to: "#{:slug}s#index"
   #match ":slug" => "home#index"
-  get ':slug/*route' => 'home#index'
+  get ':slug/*route', to: 'home#index'
 
   root :to => 'home#index'
   
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
