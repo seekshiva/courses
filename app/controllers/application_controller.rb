211616033e741ddb2a9a2785a390ab28a315f4ec
Shiva Nandan <seekshiva@gmail.com>
2013-12-26 12:06:59 +0530

    fix(CSRF) - "Rails can't verify CSRF authenticity"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,14 @@ class ApplicationController < ActionController::Base
   before_filter :setupenv
 
   def setupenv
-	@user = nil
+    @user = nil
+            
     begin
-	  unless session[:user_id].nil?
-		@user = User.find(session[:user_id])
-	  end
-	rescue
-	  # session[:user_id] not found in User table
+      unless session[:user_id].nil?
+        @user = User.find(session[:user_id])
+      end
+    rescue
+      # session[:user_id] not found in User table
     end
   end
   
