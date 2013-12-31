dee6bd16639e797e282bd74d7818cbb130d74f54
Nobelium <me@vignesh.info>
2013-12-31 14:05:10 +0000

    compiling assets for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Courses::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -57,6 +57,7 @@ Courses::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( home.css scaffolds.css uploadify.css *.js )
 
   config.action_mailer.default_url_options = { :host => "courseshub.in" }
 
