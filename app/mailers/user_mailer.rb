b934608d96db15c708b60d287d74d6fe68451c1f
Nobelium <me@vignesh.info>
2014-01-25 17:05:39 +0000

    (bug fix)- mail view bug fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,8 +5,8 @@ class UserMailer < ActionMailer::Base
     @user = user
     @host = "http://courseshub.in/"
     @faculty = nil
-    if not @user.is_student?
-      @faculty = Faculty.where user_id: @user.id
+    if !@user.is_student?
+      @faculty = Faculty.where( user_id: @user.id ).first
     end
     mail(to: "#{@user.name} <#{@user.email}@nitt.edu>", subject: "Welcome to Courseshub")
   end
