e7b9a7977eb906e43556a42cd3b39a7416a36e3d
Shiva Nandan <seekshiva@gmail.com>
2014-02-07 01:46:25 +0530

    Ignore coverage directory

--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,14 @@ ActiveRecord::Schema.define(version: 20140203203202) do
 
   create_table "authors", force: true do |t|
     t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "about"
   end
 
   create_table "avatars", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
     t.string   "pic_file_name"
     t.string   "pic_content_type"
     t.integer  "pic_file_size"
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 20140203203202) do
   create_table "book_authors", force: true do |t|
     t.integer  "book_id"
     t.integer  "author_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "book_authors", ["author_id"], name: "index_book_authors_on_author_id", using: :btree
@@ -41,8 +41,8 @@ ActiveRecord::Schema.define(version: 20140203203202) do
 
   create_table "book_covers", force: true do |t|
     t.integer  "uploaded_by"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.string   "cover_file_name"
     t.string   "cover_content_type"
     t.integer  "cover_file_size"
@@ -55,8 +55,8 @@ ActiveRecord::Schema.define(version: 20140203203202) do
     t.string   "edition"
     t.string   "isbn"
     t.integer  "year"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
     t.string   "online_retail_url"
     t.integer  "book_cover_id"
     t.integer  "file_id"
@@ -65,11 +65,11 @@ ActiveRecord::Schema.define(version: 20140203203202) do
   create_table "class_topics", force: true do |t|
     t.integer  "classroom_id"
     t.integer  "topic_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
-  add_index "class_topics", ["classroom_id"], name: "index_class_topics_on_classroom_id", using: :btree
+  add_index "class_topics", ["classroom_id"], name: "index_class_topics_on_class_id", using: :btree
   add_index "class_topics", ["topic_id"], name: "index_class_topics_on_topic_id", using: :btree
 
   create_table "classrooms", force: true do |t|
@@ -77,8 +77,8 @@ ActiveRecord::Schema.define(version: 20140203203202) do
     t.date     "date"
     t.string   "time"
     t.string   "room"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "classrooms", ["term_id"], name: "index_classrooms_on_term_id", using: :btree
@@ -87,24 +87,24 @@ ActiveRecord::Schema.define(version: 20140203203202) do
     t.string   "subject_code"
     t.string   "name"
     t.integer  "credits"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.text     "about"
   end
 
   create_table "departments", force: true do |t|
     t.string   "name"
     t.string   "short"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.string   "rollno_prefix"
     t.integer  "hod_id"
   end
 
   create_table "documents", force: true do |t|
     t.integer  "uploaded_by"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
     t.string   "document_file_name"
     t.string   "document_content_type"
     t.integer  "document_file_size"
@@ -116,8 +116,8 @@ ActiveRecord::Schema.define(version: 20140203203202) do
     t.integer  "user_id"
     t.string   "prefix"
     t.string   "designation"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.text     "about"
   end
 
@@ -127,40 +127,40 @@ ActiveRecord::Schema.define(version: 20140203203202) do
     t.integer  "term_reference_id"
     t.integer  "topic_id"
     t.string   "indices"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
-  add_index "references", ["term_reference_id"], name: "index_references_on_term_reference_id", using: :btree
+  add_index "references", ["term_reference_id"], name: "index_references_on_course_reference_id", using: :btree
   add_index "references", ["topic_id"], name: "index_references_on_topic_id", using: :btree
 
   create_table "section_documents", force: true do |t|
     t.integer  "document_id"
     t.integer  "section_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "sections", force: true do |t|
     t.integer  "term_id"
     t.string   "title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "subscriptions", force: true do |t|
     t.integer  "term_id"
     t.integer  "user_id"
     t.boolean  "attending"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "term_departments", force: true do |t|
     t.integer  "term_id"
     t.integer  "department_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   add_index "term_departments", ["department_id"], name: "index_term_departments_on_department_id", using: :btree
@@ -169,15 +169,15 @@ ActiveRecord::Schema.define(version: 20140203203202) do
   create_table "term_documents", force: true do |t|
     t.integer  "document_id"
     t.integer  "term_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "term_faculties", force: true do |t|
     t.integer  "term_id"
     t.integer  "faculty_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "term_faculties", ["faculty_id"], name: "index_term_faculties_on_faculty_id", using: :btree
@@ -186,19 +186,19 @@ ActiveRecord::Schema.define(version: 20140203203202) do
   create_table "term_references", force: true do |t|
     t.integer  "term_id"
     t.integer  "book_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "term_references", ["book_id"], name: "index_term_references_on_book_id", using: :btree
-  add_index "term_references", ["term_id"], name: "index_term_references_on_term_id", using: :btree
+  add_index "term_references", ["book_id"], name: "index_course_references_on_book_id", using: :btree
+  add_index "term_references", ["term_id"], name: "index_course_references_on_course_id", using: :btree
 
   create_table "terms", force: true do |t|
     t.integer  "course_id"
     t.integer  "academic_year"
     t.integer  "semester"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.string   "venue"
   end
 
@@ -207,15 +207,15 @@ ActiveRecord::Schema.define(version: 20140203203202) do
   create_table "topic_documents", force: true do |t|
     t.integer  "document_id"
     t.integer  "topic_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "topics", force: true do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
     t.string   "ct_status",   default: ""
     t.integer  "section_id"
   end
@@ -225,8 +225,8 @@ ActiveRecord::Schema.define(version: 20140203203202) do
     t.string   "email",                  default: "",    null: false
     t.integer  "department_id"
     t.boolean  "activated"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
     t.string   "phone"
     t.integer  "avatar_id"
