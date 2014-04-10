1166cf4562df4f6e749877028f9c9c69e4cc4b89
Shiva Nandan <seekshiva@gmail.com>
2014-04-10 06:08:04 +0000

    Convert specs to RSpec 2.14.7 syntax with Transpec
    This conversion is done by Transpec 1.12.0 with the following command:
        transpec
    
    * 7 conversions
        from: obj.should
          to: expect(obj).to

--- spec/routing/admin/documents_routing_spec.rb
+++ spec/routing/admin/documents_routing_spec.rb
@@ -4,31 +4,31 @@ describe Admin::DocumentsController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/admin/documents").should route_to("admin/documents#index")
+      expect(get("/admin/documents")).to route_to("admin/documents#index")
     end
 
     it "routes to #new" do
-      get("/admin/documents/new").should route_to("admin/documents#new")
+      expect(get("/admin/documents/new")).to route_to("admin/documents#new")
     end
 
     it "routes to #show" do
-      get("/admin/documents/1").should route_to("admin/documents#show", :id => "1")
+      expect(get("/admin/documents/1")).to route_to("admin/documents#show", :id => "1")
     end
 
     it "routes to #edit" do
-      get("/admin/documents/1/edit").should route_to("admin/documents#edit", :id => "1")
+      expect(get("/admin/documents/1/edit")).to route_to("admin/documents#edit", :id => "1")
     end
 
     it "routes to #create" do
-      post("/admin/documents").should route_to("admin/documents#create")
+      expect(post("/admin/documents")).to route_to("admin/documents#create")
     end
 
     it "routes to #update" do
-      put("/admin/documents/1").should route_to("admin/documents#update", :id => "1")
+      expect(put("/admin/documents/1")).to route_to("admin/documents#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      delete("/admin/documents/1").should route_to("admin/documents#destroy", :id => "1")
+      expect(delete("/admin/documents/1")).to route_to("admin/documents#destroy", :id => "1")
     end
 
   end
